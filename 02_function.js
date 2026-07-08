5618580901f25341aeaefc16f52d50750ea05257
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-07-08 23:20:59 +0530

    about function

--- 02_function.js
+++ 02_function.js
@@ -1,3 +1,10 @@
+/*
+    What is a Function?
+
+        A function is a reusable block of code that performs a specific task.
+        Instead of writing the same code multiple times, you write it once and call it whenever you need it.
+*/
+
 function greet(name){
     console.log(name)
 }
@@ -9,3 +16,15 @@ function calculator(a,b){
 }
 let add = calculator(10,20);
 console.log(add)
+
+function userLoggedin(user){
+    console.log(`${user}, just logged in`)
+}
+userLoggedin("Shifaali")
+
+//Arrow function
+
+const data =(name)=>{
+    console.log(name)
+}
+data("shivam_bhske")
