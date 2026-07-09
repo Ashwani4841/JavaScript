9274a606fc205f071dfb022aa69d4a077d8600fa
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-07-09 23:43:22 +0530

    about scope

--- 03_scope.js
+++ 03_scope.js
@@ -1,3 +1,5 @@
+//Global Scope
+//A variable declared outside any function or block is in the global scope.
 let name = "Ashwani";
 
 function greet(){
@@ -5,5 +7,16 @@ function greet(){
 }
 
 greet()
-
 console.log(name)
+
+//Function (Local) Scope
+//Variables declared inside a function can only be used inside that function.
+
+function greet() {
+    let message = "Hello";
+    console.log(message);
+}
+
+greet();
+
+console.log(message);
