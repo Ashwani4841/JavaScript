fa94b6abc6bdf07bddebb6dbc9b9dfed79b5a64b
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-07-13 23:04:43 +0530

    About Conditionals

---
+++ 10_Conditionals.js
@@ -0,0 +1,55 @@
+// if-else
+let age = 19;
+if(age<18){
+    console.log("You are not aligible for vote")
+}else{
+    console.log("Aligible for vote")
+}
+
+// else-if
+let marks = 56;
+if(marks>=80){
+    console.log("Grade A")
+}else if(marks>=60){
+    console.log("Grade B")
+}else if(marks>=35){
+    console.log("Grade C")
+}else{
+    console.log("Fail")
+}
+
+// nested if
+let isLoggedin = true;
+// let isAdmin = true
+
+// if(isLoggedin){
+//     if(!isAdmin){
+//         console.log("Welcome Admin")
+//     }else{
+//         console.log("login first")
+//     }
+// }
+
+let isAdmin = false;
+
+if (!isAdmin) {
+    console.log("Access Denied");
+}
+
+let hasMoney = false;
+
+if (!hasMoney) {
+    console.log("You cannot buy this product.");
+}
+
+let isStudent = true;
+
+if (!isStudent) {
+    console.log("Not a student");
+} else {
+    console.log("Student");
+}
+
+let isOnline = false;
+
+console.log(!isOnline);
