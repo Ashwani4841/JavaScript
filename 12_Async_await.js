2cd20f828202472ff14524c177c91093e53aea0e
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-07-17 23:33:59 +0530

    About Async/await

--- 12_Async_await.js
+++ 12_Async_await.js
@@ -187,3 +187,13 @@ const getData = async () => {
 console.log(1);
 getData();
 console.log(2);
+
+async function fetchData() {
+  try {
+    let response = await fetch('https://api.example.com/data');
+    let data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
