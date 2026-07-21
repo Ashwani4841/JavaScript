4f24336a32f0e798645c8938e2291626cf4def5f
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-07-21 23:57:46 +0530

    About Error handling

--- 13_Error_handling.js
+++ 13_Error_handling.js
@@ -144,3 +144,15 @@ async function fetchUserData() {
 
 fetchUserData();
 
+
+try {
+    let res = 10 / 0;
+    if (!isFinite(res)) {
+        throw new Error("Cannot divide by zero");
+    }
+    console.log(res);
+} catch (error) {
+    console.error("Error occurred:", error.message);
+} finally {
+    console.log("Execution completed");
+}
