8b9ca2a190cbc8137a5adbf49a6b7b9594e68221
Ashwani Giri <ashwanigiri7236@gmail.com>
2026-07-31 23:58:44 +0530

    About Fetch

--- 14_Fetch.js
+++ 14_Fetch.js
@@ -95,3 +95,9 @@ async function getData() {
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
+     const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
